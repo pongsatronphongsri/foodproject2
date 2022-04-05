6ddbd13a6f78b781f6854020bf64605c7495f1b7
pongsatronphongsri <6306021622011@fitm.kmutnb.ac.th>
2022-04-05 17:24:08 +0700

    update

---
+++ function.js
@@ -0,0 +1,13 @@
+var swiper = new Swiper(".home-slider", {
+    spaceBetween: 105,
+    centeredSlides: true,
+    autoplay: {
+      delay: 3000,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    loop:true,
+  }) 
--- main.js
+++ main.js
@@ -20,3 +20,5 @@ const app = Vue.createApp({
    
     
 })
+
+
