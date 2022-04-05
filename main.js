ffd638d05dbe3eeae8587726782e873404286e69
pongsatronphongsri <6306021622011@fitm.kmutnb.ac.th>
2022-04-05 16:37:40 +0700

    update

--- main.js
+++ main.js
@@ -1,7 +1,16 @@
 const app = Vue.createApp({
     data() {
         return {
-            images: 'assets/images/dish1.png'
+            images1: 'assets/images/home-food1.png',
+            images2: 'assets/images/home-food2.png',
+            images3: 'assets/images/home-food3.png',
+            images4: 'assets/images/dish1.png',
+            images5: 'assets/images/dish2.png',
+            images6: 'assets/images/dish3.png',
+            images7: 'assets/images/dish4.png',
+            images8: 'assets/images/dish5.png',
+            images9: 'assets/images/dish6.png',
+            
             
             
         }
