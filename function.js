e1c1923bf3eafb28c9557641481707718e22347c
pongsatronphongsri <6306021622011@fitm.kmutnb.ac.th>
2022-04-05 19:10:43 +0700

    update

--- function.js
+++ function.js
@@ -11,3 +11,13 @@ var swiper = new Swiper(".home-slider", {
     },
     loop:true,
   }) 
+
+
+document.querySelector('#search-icon').onclick = () =>{
+    document.querySelector('#search-form').classList.toggle('active');
+}
+  
+document.querySelector('#close').onclick = () =>{
+    document.querySelector('#search-form').classList.remove('active');
+}
+
